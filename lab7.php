868b76cba48905171099a53c91ad954fcdcba85b
who562 <81339060+who562@users.noreply.github.com>
2021-03-25 22:29:23 -0700

    latest update

--- lab7.php
+++ lab7.php
@@ -1,12 +1,12 @@
-
 <head>
- <title>Registration For Race</title>
+ <title>Thank you for Registering</title>
  <style type="text/css">
   h1 { color:  red ; }
   h2 { font-family: Times New Roman; color: blue ; }
   p { font-family: Ariel; color: green}
   input[type=submit] {
-     background-color: #4CAF50; /* Green */
+     background-image: url('/run.jpg');
+  background-color: #4CAF50; /* Green */
      border: none;
      color: white;
      padding: 20px;
@@ -20,28 +20,29 @@
  </style>
 </head>
 <body>
- <h1>Registration For Race</h1>
+ <h1>Thank you for Registering</h1>
 <?php
- if ((isset($_POST['name'])) && (isset($_POST['address'])) && (isset($_POST['gender'])) && (isset($_POST['age'])) && (isset($_POST['size'])) ) 
+ if ((isset($_POST['name'])) or (isset($_POST['address'])) or (isset($_POST['gender'])) or (isset($_POST['age'])) or (isset($_POST['size'])) ) 
        {
   $name = filter_var( $_POST['name'], FILTER_SANITIZE_STRING);
   $age = filter_var( $_POST['age'], FILTER_SANITIZE_STRING);
   $address = filter_var( $_POST['address'], FILTER_SANITIZE_STRING);
   $gender = filter_var( $_POST['gender'], FILTER_SANITIZE_STRING);
   $size = filter_var( $_POST['size'], FILTER_SANITIZE_STRING);
-  echo "<p>The original value is " . $age . " " . $address . "</p>" ;
+
   
   $cost = 25.00;
-  
-  if ($size === "2x" || $size === "3x") {
+ {
+  if ($size === "2x" || $size === "3x") 
+  {
   $cost += 2.00;
-  } 
+  }
   
   if ($age >= 65) {
     $cost -= 5.00;
+       }
   }
-  
-  echo"<p style='color: darkblue;'>Your total is $" . $cost . "</p>";
- }
+  echo"<p style='color: darkblue;'>Your total is $" .$cost. "</p>";
+        }
 ?>
  </body>
